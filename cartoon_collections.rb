a121f60abb3cd4db9e418e9cb60e1d3a34f40700
Jun Liang <illuminatemynight@gmail.com>
2019-08-05 00:33:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ end
 
 def long_planeteer_calls(calls)
   calls.each_with_index { |item, index|
-    if item.length > 4
-      return true
-    else
-      return false
+    if item.length > 4 
+      return true 
+    end 
   }
+  return false 
 end
